5219b8ce6ba54a4d485c5f28c3539566d07bdf48
Aqsa Cendekia <cendekiaqsa@gmail.com>
2023-03-03 11:10:51 +0700

    commit v1.2

--- looping.py
+++ looping.py
@@ -1,5 +1,8 @@
-for i in range(1, 101):
-    if i % 2 == 0:
-        continue
-    else:
-        print(i)
+def print_odd_numbers():
+    for i in range(1, 101):
+        if i % 2 == 0:
+            continue
+        else:
+            print(i)
+
+print_odd_numbers()
