9d75947eb2dee2dec7a1d9df008bd4aa963b36e4
Aqsa Cendekia <cendekiaqsa@gmail.com>
2023-03-10 10:46:28 +0700

    commit v1.2

--- tugas3.py
+++ tugas3.py
@@ -1,29 +1,10 @@
-arsene_lupin = {
-    'nama': 'Arsene Lupin',
-    'nik': 1234,
-    'jenis_kelamin': 'male',
-    'tanggal_lahir': '1902-03-23'
-}
-
-sherlock_holmes = {
-    'nama': 'Sherlock Holmes',
-    'nik': 4321,
-    'jenis_kelamin': 'male',
-    'tanggal_lahir': '1876-08-16'
-}
-
-irene_adler = {
-    'nama': 'Irene Adler',
-    'nik': 6789,
-    'jenis_kelamin': 'female',
-    'tanggal_lahir': '1884-10-07'
-}
-
-# membuat dictionary kosong
-orang = []
-
+orang = [
+{"nama": "arsene lupin", "nik": 1234, "jenis_kelamin": "male", "tanggal_lahir": "1902-03-23"},
+{"nama": "sherlock holmes", "nik": 4321, "jenis_kelamin": "male", "tanggal_lahir": "1876-08-16"},
+{"nama": "irene adler", "nik": 6789, "jenis_kelamin": "female", "tanggal_lahir": "1884-10-07"}
+]
 # menerima input dari user dan menambahkan ke dalam dictionary
-for i in range(3):
+for i in range(1):
     nama = input(f"Masukkan nama orang ke-{i+1}: ")
     nik = int(input(f"Masukkan NIK orang ke-{i+1}: "))
     jenis_kelamin = input(f"Masukkan jenis kelamin orang ke-{i+1} (male/female): ")
@@ -40,4 +21,17 @@ for i in range(3):
     # menambahkan dictionary orang ke dalam list orang
     orang.append(data_orang)
 
-print(orang)
+    # menerima input dari user
+nama_dicari = input("Masukkan nama yang ingin dicari: ")
+
+# melakukan pencarian berdasarkan nama
+for data in orang:
+    if data['nama'].lower() == nama_dicari.lower():
+        print(f"Data ditemukan:")
+        print(f"Nama: {data['nama']}")
+        print(f"NIK: {data['nik']}")
+        print(f"Jenis kelamin: {data['jenis_kelamin']}")
+        print(f"Tanggal lahir: {data['tanggal_lahir']}")
+        break
+else:
+    print("Data tidak ada")
